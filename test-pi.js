00477486dbcae2d8228ec79602227186e3c7bae1
yeh2020 <86224852+yeh2020@users.noreply.github.com>
2021-07-08 14:04:41 +0800

    Update test-pi.js

--- test-pi.js
+++ test-pi.js
@@ -8,8 +8,8 @@ async function init(){
 		Pi.authenticate(scopes, onOpenPaymentFound).then(function(auth){
 			console.log(`Hello ${auth.user.username}`);
 			user = auth.user.username;
-			document.getElementById('showuser').innerHTML = auth.user.username;
-			alert('user');
+			const su = document.getElementById('showuser');
+			su.innerHTML = auth.user.username;
 		})
 	} catch (err) {
 		alert(err);
