3c2d104214c4fb3e1b33f3e1bf6cb60f7359957c
yeh2020 <86224852+yeh2020@users.noreply.github.com>
2021-07-11 13:44:56 +0800

    Update test-pi.js

--- test-pi.js
+++ test-pi.js
@@ -18,16 +18,16 @@ async function init(){
 
 function payment() {
 	try {
-			const payment = Pi.createPayment({
-				amount: 3.14,
-				memo: "3.14 test-pi for Donate",
-				metadata: { orderId: 1234, itemIds: [11, 42, 314] },
-            }, 
+		const payment = Pi.createPayment({
+			amount: 3.14,
+			memo: "3.14 test-pi for Donate",
+			metadata: { orderId: 1234, itemIds: [11, 42, 314] },
+		}, 
             {
-				onReadyForServerApproval: onPaymentIdReceived,
-				onReadyForServerCompletion: onTransactionSubmitted,
-				onCancel: onPaymentCancelled,
-				onError: onPaymentError,
+		onReadyForServerApproval: onPaymentIdReceived,
+		onReadyForServerCompletion: onTransactionSubmitted,
+		onCancel: onPaymentCancelled,
+		onError: onPaymentError,
             });
 	}catch(err){
 		alert(err);
@@ -36,19 +36,36 @@ function payment() {
 
 function onPaymentIdReceived(paymentId){
 	$.ajax({
+		type: 'POST',		
+		url: 'https://api.minepi.com/v2/payments/' + {paymentID} + '/approv',
+		Authorization: ${{ secrets.PI_API_KEY }},
+		dataType: 'json',
+		memo: 'you will pay pi coin', 
+		to_address:'GCG3PKMJ6GZAYNB33ZLSCD2OROSMFROE4TFRZGA2R7GBYN7DJQ4H3GT3',//federal boat
+		status: {developer_approved: true},
+	});
+	$.ajax({
 		type: 'POST',
-        url: 'paymentId.txt',
-        dataType: 'json',
-        data: {t1:user,t2:paymentId}
+		url: 'paymentId.txt',
+		dataType: 'json',
+		data: {t1:user,t2:paymentId}
 	});
 }
 
-function onTransactionSubmitted(pid,txid){   
+function onTransactionSubmitted(pid,txid){
 	$.ajax({
-        type: 'POST',
-        url: 'txid.txt',
-        dataType: 'json',
-        data: {user:user,pid:pid,txid:txid}
+		type: 'post',		
+		url: 'https://api.minepi.com/v2/payments/' + {paymentID} + '/complete',
+		Authorization: ${{ secrets.PI_API_KEY }},
+		dataType: 'json',
+		memo: 'Payment successful', 
+		status: {developer_complete: true},
+	});
+	$.ajax({
+		type: 'POST',
+		url: 'txid.txt',
+		dataType: 'json',
+		data: {user:user,pid:pid,txid:txid}
 	});   
 }
 
