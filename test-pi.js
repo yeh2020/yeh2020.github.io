9ecaf9370eda80a08a5ae0820786fdae5ede70dc
yeh2020 <86224852+yeh2020@users.noreply.github.com>
2021-07-11 13:25:55 +0800

    Add files via upload

--- test-pi.js
+++ test-pi.js
@@ -1,7 +1,6 @@
 // Pi Network API
 
 const scopes = ['username', 'payments'];
-const Base = 'https://api.minepi.com/v2'
 var user,amount;
 async function init(){
 	try {
@@ -20,8 +19,8 @@ async function init(){
 function payment() {
 	try {
 			const payment = Pi.createPayment({
-				amount: 0.01,
-				memo: "0.01 test-pi for Donate",
+				amount: 3.14,
+				memo: "3.14 test-pi for Donate",
 				metadata: { orderId: 1234, itemIds: [11, 42, 314] },
             }, 
             {
@@ -36,61 +35,22 @@ function payment() {
 }
 
 function onPaymentIdReceived(paymentId){
-	if (status.developer_approved) {
-		ServerApproval();
-		$.ajax({
-			type: 'POST',
-			url: 'paymentId.txt',
-			dataType: 'json',
-			data: {t1:user,t2:paymentId}
-			github_token: ${{ secrets.PI_SERVER_API_KEY }},
-		});
-	}else {init()}
+	$.ajax({
+		type: 'POST',
+        url: 'paymentId.txt',
+        dataType: 'json',
+        data: {t1:user,t2:paymentId}
+	});
 }
 
-function onTransactionSubmitted(pid,txid){
-	if (transaction !== null) {
-		ServerComplet();
-		$.ajax({
-			type: 'POST',
-			url: 'txid.txt',
-			dataType: 'json',
-			data: {user:user,pid:pid,txid:txid}
-			github_token: ${{ secrets.PI_SERVER_API_KEY }},
-		}); 
-	}else {init()}
+function onTransactionSubmitted(pid,txid){   
+	$.ajax({
+        type: 'POST',
+        url: 'txid.txt',
+        dataType: 'json',
+        data: {user:user,pid:pid,txid:txid}
+	});   
 }
 
 function onPaymentCancelled(){}
 function onPaymentError(){}
-
-
-// server
-
-function ServerApproval(){
-	$.ajax({
-		type: 'post',		
-        url: base+'/payments/' + {paymentID} + '/approv',
-		Authorization: ${{ secrets.PI_API_KEY }},
-		dataType: 'json',
-		user_uid:,
-		amount: 0.01,
-		memo: 'you will pay pi coin', 
-		to_address:'GCG3PKMJ6GZAYNB33ZLSCD2OROSMFROE4TFRZGA2R7GBYN7DJQ4H3GT3',//federal boat
-		created_at:,
-		status: {developer_approved: true},
-	});
-}
-
-function ServerComplet(){
-	$.ajax({
-		type: 'post',		
-        url: base+'/payments/'+{paymentID}+'/complete',
-		Authorization: ${{ secrets.PI_API_KEY }},
-		dataType: 'json',
-		user_uid:,
-		memo: 'Payment successful', 
-		created_at:,
-		status: {developer_complete: true},
-	});
-}
