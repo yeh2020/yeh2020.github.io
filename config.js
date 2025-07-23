6545587888be014000bfa4a6653abba7cb9bc9a0
yeh2020 <86224852+yeh2020@users.noreply.github.com>
2025-07-23 22:27:23 +0800

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// 敏感數據配置文件
-// 注意：此文件包含敏感信息，不應該提交到公共儲存庫
-// 將此文件添加到 .gitignore
-
-const CONFIG = {
-    // Pi 錢包地址
-    WALLET_ADDRESS: "GBFSZO7SFB6LBAN62DSWG56I7L2QYUUHT64R43IADIHEY2NRG3QVKT4W"
-};
